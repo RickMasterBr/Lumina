a9f672b4f075494ef4903445e5b8bd71f6b7653f
Richard <richardlopesstrougo@gmail.com>
2025-04-28 20:30:48 -0300

    setup inicial do site Lumina

---
+++ src/app/podcast/page.jsx
@@ -0,0 +1,8 @@
+export default function Podcast() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Podcast</h1>
+      <p className="text-lg">Welcome to the podcast page!</p>
+    </div>
+  );
+}
---
+++ src/app/projetos/page.jsx
@@ -0,0 +1,8 @@
+export default function Projetos() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Projetos</h1>
+      <p className="text-lg">Bem-vindo à página de projetos!</p>
+    </div>
+  );
+}
