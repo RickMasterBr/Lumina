86d09559e8b1bb6fcb2cb17c7b968fb4f66ccdd5
Richard <richardlopesstrougo@gmail.com>
2025-04-28 20:37:32 -0300

    Setup inicial

--- src/app/projetos/page.jsx
+++ src/app/projetos/page.jsx
@@ -1,7 +1,7 @@
 export default function Projetos() {
   return (
     <div className="flex flex-col gap-4">
-      <h1 className="text-2xl font-bold">Projetos</h1>
+      <h1 className="text-2xl font-bold text-purple-500">Projetos</h1>
       <p className="text-lg">Bem-vindo à página de projetos!</p>
     </div>
   );
