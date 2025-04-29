00d066404e780674a18562fc7135ac25d1117fe4
Richard <richardlopesstrougo@gmail.com>
2025-04-28 21:17:00 -0300

    Feat: Adição do firebase hsoting

--- src/app/projetos/page.jsx
+++ src/app/projetos/page.jsx
@@ -1,7 +1,7 @@
 export default function Projetos() {
   return (
-    <div className="flex flex-col gap-4">
-      <h1 className="text-2xl font-bold text-purple-500">Projetos</h1>
+    <div className="flex flex-col gap-4 p-4 text-center">
+      <h1 className="text-7xl font-bold text-purple-500">Projetos</h1>
       <p className="text-lg">Bem-vindo à página de projetos!</p>
     </div>
   );
